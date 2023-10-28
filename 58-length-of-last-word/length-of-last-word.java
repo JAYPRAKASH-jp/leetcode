0107669b9eda6b7a03a2721e0ff0e2385a1f5487
JAYPRAKASH-jp <84801335+JAYPRAKASH-jp@users.noreply.github.com>
2023-10-28 21:06:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (78.69%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int count = 0;
+        // Start Loop from last index if the last index is not space then start length count once space found the if count is not 0 and then return count.
         for(int i = s.length() - 1 ; i > -1 ; i--){
             if(s.charAt(i) != ' '){
                 count++;
