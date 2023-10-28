fc35e9d1cadf93bdea069ee26ca1e42a1c5134d6
JAYPRAKASH-jp <84801335+JAYPRAKASH-jp@users.noreply.github.com>
2023-10-28 20:18:33 +0530

    Time: 2 ms (76.03%) | Memory: 60.9 MB (49.28%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,8 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        //Initialize profit as 0
         int profit = 0;
+        //Initialize Buy Price as Day 1 price
         int bp = prices[0];
         for(int i=0; i < prices.length; i++){
+            //If the next day price comes down then change the bp to low bp else if bp - latest price is > profit then chage the profit
             if(prices[i] < bp){
                 bp = prices[i];
             }else if (prices[i] - bp > profit){
